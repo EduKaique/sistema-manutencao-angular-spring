e80d6b1da36b94d04e552b4f7892db657ab96f90
mariaconradtech <mariajuliaandradeconrad@gmail.com>
2025-08-19 15:02:56 -0300

    Solicitação de manutenção - RF004

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NewRequestPageComponent } from './features/client/new-request-page/new-request-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'new-request',
+        component: NewRequestPageComponent
+    }
+];
