9bbe2c381cf9d95934b8f36decceee7c8b0809af
pompssss <rafaelluispomponio@gmail.com>
2025-08-18 22:30:34 -0300

    commit da estrutura da pagina de entrega de orçamento

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './core/auth/pages/login-page/login-page.component';
 import { PaymentPanelComponent } from './features/client/request-detail-page/components/payment-panel/payment-panel.component';
+import { BudgetDeliveryComponent } from './features/employee/budget-delivery/budget-delivery.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
     path: 'login',
     component: LoginPageComponent,
   },
+      {
+        path: 'budget-delivery',
+        component: BudgetDeliveryComponent 
+    },
   {
     path: 'payment-page',
     component: PaymentPanelComponent
