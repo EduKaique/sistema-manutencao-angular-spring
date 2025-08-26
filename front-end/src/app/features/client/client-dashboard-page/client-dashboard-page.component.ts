b9464cc887a3b173a50592f7135804c38a4938c0
Emanuelle <rodriguess.emanuelle@gmail.com>
2025-08-26 17:35:20 -0300

    feat:client-dashboard

---
+++ front-end/src/app/features/client/client-dashboard-page/client-dashboard-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-dashboard-page',
+  imports: [],
+  templateUrl: './client-dashboard-page.component.html',
+  styleUrl: './client-dashboard-page.component.css'
+})
+export class ClientDashboardPageComponent {
+
+}
