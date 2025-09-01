86f36c9fab5a27c96e9b8bb0639d3327f45686e0
xXbrasilXx <gabriel.paulabrasil@gmail.com>
2025-09-01 17:11:14 -0300

    Atualização página de pagamento e criação do componente "modal-mensagem"

--- front-end/src/app/features/client/request-detail-page/components/payment-panel/payment-panel.component.ts
+++ front-end/src/app/features/client/request-detail-page/components/payment-panel/payment-panel.component.ts
@@ -1,12 +1,34 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Pagamento {
+  titulo: string;
+  preco: number;
+}
 
 @Component({
   selector: 'app-payment-panel',
   templateUrl: './payment-panel.component.html',
+  imports: [CommonModule],
   styleUrls: ['./payment-panel.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PaymentPanelComponent {
+  backLink = '← Voltar para Página Inicial';
+  item = 'Notebook Dell Inspiron 15';
+  categoria = 'Notebook';
+  defeito = `O equipamento liga, mas a tela permanece preta (sem imagem). 
+  O LED indicador de energia acende e é possível ouvir o som da ventoinha 
+  em funcionamento, mas não há qualquer sinal de vídeo. 
+  O problema persiste mesmo após reiniciar o dispositivo várias vezes.`;
+
+  pagamentos: Pagamento[] = [
+    { titulo: 'Diagnóstico técnico', preco: 100 },
+    { titulo: 'Substituição do cabo flat da tela', preco: 180 },
+    { titulo: 'Mão de obra', preco: 150 },
+    { titulo: 'Limpeza interna + pasta térmica', preco: 90 },
+  ];
+
   selectedMethod: 'cartao' | 'pix' | null = null;
 
   selectMethod(method: 'cartao' | 'pix') {
@@ -26,4 +48,13 @@ export class PaymentPanelComponent {
       alert('Pagamento via PIX realizado!');
     }
   }
+get total() {
+  let soma = 0;
+  for (let p of this.pagamentos) {
+    soma = soma + p.preco;
+  }
+  return soma;
+}
+
+
 }
