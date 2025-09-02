c03bc451c44172ecb5221531ee2c8c4e07d4ebaa
pompssss <rafaelluispomponio@gmail.com>
2025-09-01 23:07:48 -0300

    Estilização do Header e alterações a página Budget-delivery

--- front-end/src/app/core/layout/header/header.component.ts
+++ front-end/src/app/core/layout/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar],
+  imports: [MatToolbar, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+     nomeDeUsuario: string = 'Nilson Nativas';
 }
