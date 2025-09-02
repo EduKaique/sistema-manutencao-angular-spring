ceb4514ea8a9c0a2a53fa03cd4443fbef61f8daf
mariaconradtech <mariajuliaandradeconrad@gmail.com>
2025-09-02 15:40:55 -0300

    Visualizar serviço - criação dos componentes

---
+++ front-end/src/app/features/client/request-detail-page/components/request-description/request-description.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-description',
+  imports: [],
+  templateUrl: './request-description.component.html',
+  styleUrl: './request-description.component.css'
+})
+export class RequestDescriptionComponent {
+
+}
