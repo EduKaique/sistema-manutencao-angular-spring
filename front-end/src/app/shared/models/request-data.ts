078b9711212a8ff258b9435c516328b2784e2747
mariaeferreirac <m.eduardafcarvalho@gmail.com>
2025-09-16 17:38:02 -0300

    Tela de aprovar/reprovar orçamento com LOcal Storage

---
+++ front-end/src/app/shared/models/request-data.ts
@@ -0,0 +1,14 @@
+export interface RequestData {
+  id: number;
+  equipmentName: string;
+  equipmentDescription: string;
+  requestDate: Date;
+  statusId: number;
+  categoryId: number;
+  clientId: number;
+  employeeId: number;
+  valor: number;
+  status: 'APROVADA' | 'REJEITADA' | string;
+  prazo?: string; 
+  servicos?: string;
+}
