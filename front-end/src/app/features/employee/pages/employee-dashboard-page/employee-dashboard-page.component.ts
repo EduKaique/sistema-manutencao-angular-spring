0ce90ede7bf9aa11313f59729cc3e7130f9081c7
Emanuelle <rodriguess.emanuelle@gmail.com>
2025-09-30 08:40:21 -0300

    Atualizações

--- front-end/src/app/features/employee/pages/employee-dashboard-page/employee-dashboard-page.component.ts
+++ front-end/src/app/features/employee/pages/employee-dashboard-page/employee-dashboard-page.component.ts
@@ -41,44 +41,17 @@ export class EmployeeDashboardPageComponent {
     }));
   });
 
-  openRequests = 4;
-  overdueRequests = 1;
-  noResponsible = 2;
-  createdToday = 3;
-
-  // requests = [
-  //   {
-  //     title: 'Notebook Dell Inspiron 15',
-  //     openedAgo: '28 horas',
-  //     category: 'Notebook',
-  //     client: 'Nilson Nativus',
-  //     status: '',
-  //   },
-  //   {
-  //     title: 'Troca de tela do celular',
-  //     openedAgo: '2 horas',
-  //     category: 'Celular',
-  //     client: 'Renata Montano',
-  //     status: '',
-  //   },
-  //   {
-  //     title: 'Formatação e backup',
-  //     openedAgo: '6 minutos',
-  //     category: 'Sistema Operacional',
-  //     client: 'Floury Nations',
-  //     status: 'Sem Responsável',
-  //   },
-  //   {
-  //     title: 'Teclado Mecânico',
-  //     openedAgo: '4 minutos',
-  //     category: 'Equipamento',
-  //     client: 'Razet Montano',
-  //     status: 'Sem Responsável',
-  //   },
-  // ];
+  
+  overdueRequests = 0;
+  noResponsible = 0;
+  createdToday = 0;
 
   makeQuote(request: any) {
     console.log('Efetuando orçamento para:', request.title);
-    // Aqui você pode abrir um modal ou navegar para outra página
+  
+  }
+  getOpenRequestCount(): number {
+    const requests = this.requests;
+    return requests.filter(req => req.statusId === 1).length;
   }
 }
