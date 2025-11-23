f530286f0d4fac18a58a5d62ef67fc2af21ccc0c
xXbrasilXx <gabriel.paulabrasil@gmail.com>
2025-11-23 14:59:54 -0300

    Alterações em Employee + Interceptor

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNgxMask } from 'ngx-mask';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
+import { apiInterceptorInterceptor } from './core/interceptors/api-interceptor.interceptor';
+
 
 registerLocaleData(localePt);
 
@@ -14,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNgxMask(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([apiInterceptorInterceptor])),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 };
