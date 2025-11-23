68b718ad6297c27c3cdec3b40be5bd08ffedd264
Eduardo Kaique Alberico <109116458+EduardoAlberico@users.noreply.github.com>
2025-11-23 15:19:37 -0300

    add: padronizando enviroments e acesso a api

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNgxMask } from 'ngx-mask';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
+import { API_URL } from './core/configs/api.token';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
-import { apiInterceptorInterceptor } from './core/interceptors/api-interceptor.interceptor';
 
 
 registerLocaleData(localePt);
@@ -16,7 +18,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNgxMask(),
-    provideHttpClient(withInterceptors([apiInterceptorInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([apiInterceptor])),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: API_URL, useValue: environment.apiUrl }
   ],
 };
--- front-end/src/app/core/interceptors/api.interceptor.ts
+++ front-end/src/app/core/interceptors/api.interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-export const apiInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   
-  // 1. Tenta pegar o token do localStorage
   const token = localStorage.getItem('token'); 
 
-  // 2. Se o token existir, CLONA a requisição e adiciona o cabeçalho
   if (token) {
     const authReq = req.clone({
       setHeaders: {
@@ -15,6 +13,5 @@ export const apiInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
     return next(authReq);
   }
 
-  // 3. Se não tiver token, manda a original
   return next(req);
 };
