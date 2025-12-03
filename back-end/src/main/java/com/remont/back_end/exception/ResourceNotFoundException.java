1a24777559dee89605c7d5584d7fbce528fbee40
Eduardo Kaique Alberico <109116458+EduKaique@users.noreply.github.com>
2025-12-03 18:16:08 +0000

    refactor: removendo arquivos inúteis

--- back-end/src/main/java/com/remont/back_end/exception/ResourceNotFoundException.java
+++ back-end/src/main/java/com/remont/back_end/exception/ResourceNotFoundException.java
@@ -3,8 +3,6 @@ package com.remont.back_end.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// Exceção customizada para quando um recurso não é encontrado.
-// O @ResponseStatus faz o Spring retornar um erro 404 (Not Found) automaticamente.
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 	
